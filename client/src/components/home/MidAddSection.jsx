14e19554da76bae0be961cf94fb5937c4b76454c
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2024-08-26 04:49:12 +0530

    add homeroute ./

--- client/src/components/home/MidAddSection.jsx
+++ client/src/components/home/MidAddSection.jsx
@@ -16,7 +16,7 @@ const MidAddSection = () => {
         <>
             <Container container lg={12} md={12} xs={12}  >
                 {
-                    imageURL.map(image => (
+                    imageURL?.map(image => (
                         <Grid item lg={4} md={4} sm={12} xs={12} >
 
                             <img src={image} alt="promotion" style={{ width: '100%' }} />
